6fbfd145241014c8bb4bc33ca7d5e6f1d5f2dcc6
Anoop Kumar <127828332+anu-ship-it@users.noreply.github.com>
2026-05-24 13:42:24 +0530

    Day3 String solved reverse vowels

--- Basic Data Structures/String and Array/String/Day3 String.js
+++ Basic Data Structures/String and Array/String/Day3 String.js
@@ -1,1 +1,38 @@
+// Day 3
+// Problem Statement = Given a string where we have to reverse vowels
 
+// Example
+// Input: s = "IceCreAm"
+// Output: "AceCreIm"
+// Explanation:
+// Core Problem what we have to do = 
+
+// For understanding the logic of reversing the vowels
+function reverseVowels(s) {
+    let vowels = "aeiouAEIOU";
+    let arr = s.split('');
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        while (
+            left < right &&
+            !vowels.includes(arr[left])
+        ) {
+            left++;
+        }
+        while (
+            left < right &&
+            !vowels.includes(arr[right])
+        ) {
+            right--;
+        }
+        // swap vowels
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+
+    return arr.join('');
+}
